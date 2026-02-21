bf8318eeae581bd5922b90c78897d32e5ddadebb
luizfernandonsc1-gif <luizfernandonsc1@gmail.com>
2026-02-21 18:04:47 -0300

    Add checklist routes with CRUD operations

---
+++ src/routes/checklists.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const ctrl = require('../controllers/checklistController');
+const auth = require('../middlewares/auth');
+const authorize = require('../middlewares/authorize');
+
+router.use(auth);
+
+router.get('/', ctrl.list);
+router.get('/:id', ctrl.getOne);
+router.post('/', authorize('admin'), ctrl.create);
+router.put('/:id', authorize('admin'), ctrl.update);
+router.delete('/:id', authorize('admin'), ctrl.remove);
+
+module.exports = router;
